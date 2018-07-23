d0683ffe44718c3e7c8fce67fdf56c1ff90e5682
Administrator <admin@Whydah.local>
2018-07-23 10:13:57 -0400

    Added test for the player module

---
+++ test/player_test.exs
@@ -0,0 +1,11 @@
+defmodule PlayerTest do
+  use ExUnit.Case
+  doctest Player
+
+  test "Player struct is correctly created" do
+    name = "Steven"
+    token = :x
+    type = :human
+    assert Player.new(name, token, type) == %Player{name: "Steven", token: :x, type: :human}
+  end
+end

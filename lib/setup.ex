cd55f7bf4a6c14a0cc9c97ec99277f7ea1b02d2f
Administrator <admin@Whydah.local>
2018-07-25 13:48:06 -0500

    Refectored player set process to make use of new player functions

--- lib/setup.ex
+++ lib/setup.ex
@@ -32,53 +32,56 @@ defmodule Setup do
   end
 
   def player_vs_player do
-    player1_name = set_player_name(1)
-    player2_name = set_player_name(2)
-    player1 = Player.setup(player1_name, :x, :human)
-    player2 = Player.setup(player2_name, :o, :human)
+    player1 = set_human_player(1, :x)
+    player2 = set_human_player(2, :o)
     {player1, player2}
   end
 
   def player_vs_computer do
-    player1_name = set_player_name(1)
-    player2_name = set_player_name(2)
-    computer = computer_type_menu()
-    player1 = Player.setup(player1_name, :x, :human)
-    player2 = Player.setup(player2_name, :o, computer)
+    player1 = set_human_player(1, :x)
+    player2 = set_computer_player(2, :o)
     {player1, player2}
   end
 
   def computer_vs_computer do
-    player1_name = set_player_name(1)
-    computer1 = computer_type_menu()
-    player2_name = set_player_name(2)
-    computer2 = computer_type_menu()
-    player1 = Player.setup(player1_name, :x, computer1)
-    player2 = Player.setup(player2_name, :o, computer2)
+    player1 = set_computer_player(1, :x)
+    player2 = set_computer_player(2, :o)
     {player1, player2}
   end
 
-  def computer_type_menu do
+  def set_human_player(player_number, token) do
+    human_name = set_player_name(player_number)
+    human_name
+    |> Player.human(token)
+  end
+
+  def set_computer_player(player_number, token) do
+    computer_name = set_player_name(player_number)
+    computer_name
+    |> computer_type_menu(token)
+  end
+
+  defp computer_type_menu(computer_name, token) do
       IO.puts "Please choose the type of the computer player:\n"
       IO.puts "1. Naive - (Computer will take the first available spot)"
       IO.puts "2. Random - (Computer will take an available spot randomly)\n"
       input = IO.gets "Your choice is (Enter 1 or 2): "
       input
       |> String.trim()
-      |> choose_computer_type()
+      |> choose_computer_type(computer_name, token)
   end
 
-  def choose_computer_type(choice) do
+  defp choose_computer_type(choice, computer_name, token) do
     cond do
-      choice == "1" -> :naive_computer
-      choice == "2" -> :random_computer
+      choice == "1" -> Player.naive_computer(computer_name, token)
+      choice == "2" -> Player.random_computer(computer_name, token)
       true ->
         IO.puts "\nInvalid entry. Please try again\n"
-        computer_type_menu()
+        computer_type_menu(computer_name, token)
     end
   end
 
-  def set_player_name(player_number) do
+  defp set_player_name(player_number) do
     input = IO.gets "\nPlease enter your name (Player #{player_number}): "
     name = String.trim(input)
     case valid_name?(name) do
@@ -89,7 +92,7 @@ defmodule Setup do
     end
   end
 
-  def valid_name?(name) do
+  defp valid_name?(name) do
     name != ""
   end
 end

dd9c9882dfbd3e90959fe422e044c16eca91716b
Administrator <admin@Whydah.local>
2018-07-25 13:47:19 -0500

    Added functions for each player type for creating different players

--- lib/player.ex
+++ lib/player.ex
@@ -3,7 +3,19 @@ defmodule Player do
   @enforce_keys [:name, :token, :type]
   defstruct @enforce_keys
 
-  def setup(name, token, type) do
+  defp setup(name, token, type) do
     %Player{name: name, token: token, type: type}
+  end
+
+  def human(name, token) do
+    setup(name, token, :human)
+  end
+
+  def naive_computer(name, token) do
+    setup(name, token, :naive_computer)
+  end
+
+  def random_computer(name, token) do
+    setup(name, token, :random_computer)
   end
 end

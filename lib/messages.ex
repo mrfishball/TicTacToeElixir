5e785b00546d37035bd1ea5a7ca40caaa5d72386
Administrator <admin@Whydah.local>
2018-08-07 13:09:53 -0400

    Added more message types

--- lib/messages.ex
+++ lib/messages.ex
@@ -20,17 +20,37 @@ defmodule Messages do
     "What do you want your token to be, #{player_name}?: "
   end
 
+  def game_status(outcome) do
+    "Game over!\nIt's a #{outcome}!"
+  end
+
+  def game_status(outcome, winner) do
+    "Game over!\nThe #{outcome} is #{winner}!"
+  end
+
+  def make_a_move(name, token) do
+    "#{name} - '#{String.trim(token)}', please enter a number from 1 to 9 only: "
+  end
+
+  def invalid_move(error) do
+    "\nInvalid move: #{error}. Please try again. \n"
+  end
+
+  def invalid_move do
+    "\nInvalid move. Please try again.\n"
+  end
+
   def game_menu do
     "Let's play Tic Tac Toe!\n\n" <>
     "Select a game mode (Enter a number from 1 to 3)\n\n" <>
     "1. Player vs. Player\n" <>
     "2. Player vs. Computer\n" <>
-    "3. Spectate a game\n\n"
+    "3. Spectate a game\n"
   end
 
   def computer_choice_menu(computer_name) do
-    "Choose the type of the computer player for #{computer_name} (Enter a number from 1 to 2)\n" <>
+    "\nChoose the type of the computer player for #{computer_name} (Enter a number from 1 to 2)\n\n" <>
     "1. Naive - (#{computer_name} will take the first available spot)\n" <>
-    "2. Random - (#{computer_name} will take an available spot randomly)\n\n"
+    "2. Random - (#{computer_name} will take an available spot randomly)\n"
   end
 end

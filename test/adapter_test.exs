fbac0eae31bda6d10a43901813a9403fe9690663
Administrator <admin@Whydah.local>
2018-08-07 15:48:43 -0400

    Added tests for adapter module

--- test/adapter_test.exs
+++ test/adapter_test.exs
@@ -4,11 +4,11 @@ defmodule AdapterTest do
 
   test "adapter relays payload to output source" do
     payload = "This is a payload"
-    assert Adapter.output(payload) == :ok
+    assert capture_io(fn -> Adapter.output(payload) end) == "This is a payload\n"
   end
 
   test "adapter relays requests to input source" do
     request = "How are you?"
-    assert Adapter.input(request) == :ok
+    assert capture_io("", fn -> Adapter.input(request) end) == "How are you?"
   end
 end

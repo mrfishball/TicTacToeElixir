0390edbddce3cc2dd2c66ce988359f4b3e659616
Administrator <admin@Whydah.local>
2018-08-07 14:22:10 -0400

    Use module attribute for message type

--- lib/messages.ex
+++ lib/messages.ex
@@ -1,47 +1,52 @@
 defmodule Messages do
 
+  @menu :menu
+  @request :request
+  @error :error
+  @status :status
+
   def title do
-    {:menu, "Let's play Tic Tac Toe!\n\n"}
+    {@menu, "Let's play Tic Tac Toe!\n\n"}
   end
 
   def choice do
-    {:request, "Your choice is "}
+    {@request, "Your choice is "}
   end
 
   def player_name(player_number) do
-    {:request, "What is your name? (Player #{player_number}): "}
+    {@request, "What is your name? (Player #{player_number}): "}
   end
 
   def player_symbol(player_name) do
-    {:request, "What do you want your token to be, #{player_name}?: "}
+    {@request, "What do you want your token to be, #{player_name}?: "}
   end
 
   def game_status(outcome) do
-    {:status, "Game over!\nIt's a #{outcome}!"}
+    {@status, "Game over!\nIt's a #{outcome}!"}
   end
 
   def game_status(outcome, winner) do
-    {:status, "Game over!\nThe #{outcome} is #{winner}!"}
+    {@status, "Game over!\nThe #{outcome} is #{winner}!"}
   end
 
   def make_a_move(name, token) do
-    {:request, "#{name} - '#{String.trim(token)}', please enter a number from 1 to 9 only: "}
+    {@request, "#{name} - '#{String.trim(token)}', please enter a number from 1 to 9 only: "}
   end
 
   def invalid_move(error) do
-    {:error, "\nInvalid move: #{error}. Please try again. \n"}
+    {@error, "\nInvalid move: #{error}. Please try again. \n"}
   end
 
   def invalid_move do
-    {:error, "\nInvalid move. Please try again.\n"}
+    {@error, "\nInvalid move. Please try again.\n"}
   end
 
   def invalid_entry do
-    {:error, "\nInvalid entry. Please try again.\n"}
+    {@error, "\nInvalid entry. Please try again.\n"}
   end
 
   def game_menu do
-    {:menu, "Let's play Tic Tac Toe!\n\n" <>
+    {@menu, "Let's play Tic Tac Toe!\n\n" <>
     "Select a game mode (Enter a number from 1 to 3)\n\n" <>
     "1. Player vs. Player\n" <>
     "2. Player vs. Computer\n" <>
@@ -49,7 +54,7 @@ defmodule Messages do
   end
 
   def computer_choice_menu(computer_name) do
-    {:menu, "\nChoose the type of the computer player for #{computer_name} (Enter a number from 1 to 2)\n\n" <>
+    {@menu, "\nChoose the type of the computer player for #{computer_name} (Enter a number from 1 to 2)\n\n" <>
     "1. Naive - (#{computer_name} will take the first available spot)\n" <>
     "2. Random - (#{computer_name} will take an available spot randomly)\n"}
   end

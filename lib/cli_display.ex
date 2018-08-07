a221c362b99776dbbf772e8d15062cf2fbc22fd0
Administrator <admin@Whydah.local>
2018-08-07 14:17:44 -0400

    Improve CLI output module to handle message type coloring

--- lib/cli_display.ex
+++ lib/cli_display.ex
@@ -1,11 +1,20 @@
 defmodule CLIdisplay do
 
+  def output({type, message} = _payload) do
+    case type do
+      :menu -> IO.puts Colorizer.yellow(message)
+      :error -> IO.puts Colorizer.red(message)
+      :status -> IO.puts message
+      :request -> IO.puts message
+    end
+  end
+
   def output(payload) do
     IO.puts "#{payload}"
   end
 
-  def input(message) do
-    input = IO.gets "#{message}"
+  def input({_type, message} = _payload) do
+    input = IO.gets message
     input
   end
 end

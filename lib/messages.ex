cec8b558b122dbddc82f9debbf04712cfa173581
Administrator <admin@Whydah.local>
2018-08-07 14:16:59 -0400

    message payload will include message type

--- lib/messages.ex
+++ lib/messages.ex
@@ -1,56 +1,56 @@
 defmodule Messages do
 
   def title do
-    "Let's play Tic Tac Toe!\n\n"
-  end
-
-  def error do
-    "\nInvalid entry. Please try again.\n"
+    {:menu, "Let's play Tic Tac Toe!\n\n"}
   end
 
   def choice do
-    "Your choice is "
+    {:request, "Your choice is "}
   end
 
   def player_name(player_number) do
-    "What is your name? (Player #{player_number}): "
+    {:request, "What is your name? (Player #{player_number}): "}
   end
 
   def player_symbol(player_name) do
-    "What do you want your token to be, #{player_name}?: "
+    {:request, "What do you want your token to be, #{player_name}?: "}
   end
 
   def game_status(outcome) do
-    "Game over!\nIt's a #{outcome}!"
+    {:status, "Game over!\nIt's a #{outcome}!"}
   end
 
   def game_status(outcome, winner) do
-    "Game over!\nThe #{outcome} is #{winner}!"
+    {:status, "Game over!\nThe #{outcome} is #{winner}!"}
   end
 
   def make_a_move(name, token) do
-    "#{name} - '#{String.trim(token)}', please enter a number from 1 to 9 only: "
+    {:request, "#{name} - '#{String.trim(token)}', please enter a number from 1 to 9 only: "}
   end
 
   def invalid_move(error) do
-    "\nInvalid move: #{error}. Please try again. \n"
+    {:error, "\nInvalid move: #{error}. Please try again. \n"}
   end
 
   def invalid_move do
-    "\nInvalid move. Please try again.\n"
+    {:error, "\nInvalid move. Please try again.\n"}
+  end
+
+  def invalid_entry do
+    {:error, "\nInvalid entry. Please try again.\n"}
   end
 
   def game_menu do
-    "Let's play Tic Tac Toe!\n\n" <>
+    {:menu, "Let's play Tic Tac Toe!\n\n" <>
     "Select a game mode (Enter a number from 1 to 3)\n\n" <>
     "1. Player vs. Player\n" <>
     "2. Player vs. Computer\n" <>
-    "3. Spectate a game\n"
+    "3. Spectate a game\n"}
   end
 
   def computer_choice_menu(computer_name) do
-    "\nChoose the type of the computer player for #{computer_name} (Enter a number from 1 to 2)\n\n" <>
+    {:menu, "\nChoose the type of the computer player for #{computer_name} (Enter a number from 1 to 2)\n\n" <>
     "1. Naive - (#{computer_name} will take the first available spot)\n" <>
-    "2. Random - (#{computer_name} will take an available spot randomly)\n"
+    "2. Random - (#{computer_name} will take an available spot randomly)\n"}
   end
 end

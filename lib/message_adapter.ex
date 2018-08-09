05bb8396d4283678bfe11bf9df433f6273cfc4a8
Administrator <admin@Whydah.local>
2018-08-09 11:19:12 -0400

    New name for the adapter module

---
+++ lib/message_adapter.ex
@@ -0,0 +1,10 @@
+defmodule MessageAdapter do
+
+  def input(payload, source \\ &CLIdisplay.input/1) do
+    source.(payload)
+  end
+
+  def output(payload, source \\ &CLIdisplay.output/1) do
+    source.(payload)
+  end
+end
---
+++ test/message_adapter_test.exs
@@ -0,0 +1,14 @@
+defmodule MessageAdapterTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "adapter relays payload to output source" do
+    payload = "This is a payload"
+    assert capture_io(fn -> MessageAdapter.output(payload) end) == "This is a payload\n"
+  end
+
+  test "adapter relays requests to input source" do
+    request = "How are you?"
+    assert capture_io("", fn -> MessageAdapter.input(request) end) == "How are you?"
+  end
+end

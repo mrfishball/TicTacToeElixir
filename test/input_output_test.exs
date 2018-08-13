2259291bbdeadd65434209f56f6af2882a2ee55a
Administrator <admin@Whydah.local>
2018-08-13 11:48:15 -0400

    Fixed failed tests

--- test/input_output_test.exs
+++ test/input_output_test.exs
@@ -9,6 +9,6 @@ defmodule MessageAdapterTest do
 
   test "adapter relays requests to input source" do
     request = "How are you?"
-    assert capture_io("", fn -> InputOutput.input(request) end) == "How are you?"
+    assert capture_io(" ", fn -> InputOutput.input(request) end) == "How are you?"
   end
 end

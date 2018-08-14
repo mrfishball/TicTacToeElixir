0d67c225af4f4e6b24be524e066df58333dca861
Administrator <admin@Whydah.local>
2018-08-14 15:15:00 -0400

    fixed cli tests

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,25 +1,30 @@
 defmodule CliTest do
+  alias TTT.Console.Cli, as: Cli
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  test "menu message will be yellow" do
-    {type, message} = {:menu, "this is a menu"}
-    assert capture_io(fn -> Cli.output({type, message}) end) == "\e[33mthis is a menu\e[0m\n"
+  test "menu type message will be yellow" do
+    type = MessageFlags.menu()
+    message = "this is a menu"
+    assert capture_io(fn -> Cli.output(message, type) end) == "\e[33mthis is a menu\e[0m\n"
   end
 
   test "status message will be yellow" do
-    {type, message} = {:status, "this is a status"}
-    assert capture_io(fn -> Cli.output({type, message}) end) == "\e[33mthis is a status\e[0m\n"
+    type = MessageFlags.status()
+    message = "this is a status"
+    assert capture_io(fn -> Cli.output(message, type) end) == "\e[33mthis is a status\e[0m\n"
   end
 
   test "error message will be red" do
-    {type, message} = {:error, "this is an error"}
-    assert capture_io(fn -> Cli.output({type, message}) end) == "\e[31mthis is an error\e[0m\n"
+    type = MessageFlags.error()
+    message = "this is an error"
+    assert capture_io(fn -> Cli.output(message, type) end) == "\e[31mthis is an error\e[0m\n"
   end
 
   test "request message will be yellow" do
-    {type, message} = {:request, "this is a request"}
-    assert capture_io(" ", fn -> Cli.input({type, message}) end) == "\e[33mthis is a request\e[0m"
+    type = MessageFlags.request()
+    message = "this is a request"
+    assert capture_io(" ", fn -> Cli.input(message, type) end) == "\e[33mthis is a request\e[0m"
   end
 
   test "message with no type field will be of default color" do
@@ -27,7 +32,7 @@ defmodule CliTest do
     assert capture_io(fn -> Cli.output(payload) end) == "this is just a message\n"
   end
 
-  test "reuest with no type field will be of default color" do
+  test "request with no type field will be of default color" do
     payload = "this is just a request"
     assert capture_io(" ", fn -> Cli.input(payload) end) == "this is just a request"
   end

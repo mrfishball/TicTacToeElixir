b76ffddb1f502addbe97897dfbda50ad9b5f494c
Administrator <admin@Whydah.local>
2018-08-14 15:40:38 -0400

    simplified game runner

--- lib/game_runner.ex
+++ lib/game_runner.ex
@@ -5,20 +5,8 @@ defmodule GameRunner do
   end
 
   def new_game do
-    GameMaker.game_menu
-    |> start_game()
-  end
-
-  def start_game(players) do
-    player = GameMaker.longest_token_player(players)
-    {player_one, player_two} = GameMaker.add_paddings(player, players)
-    {left_pad, right_pad} = GameMaker.symbol_paddings(player.token, " ")
-
-    game = Game.new_game(player_one, player_two, String.length(player.token))
-    board = Board.new_board(left_pad, right_pad)
-    status = Game.status(game)
-    Board.show(board, String.length(player.token))
-
-    TTT.play(board, game, status, player_one)
+    GameMaker.show_game_menu()
+    |> GameMaker.assemble_game()
+    |> TTT.play()
   end
 end

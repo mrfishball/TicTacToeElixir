312fa32171ca58c37a8745ca54eb0af284d62012
Administrator <admin@Whydah.local>
2018-08-14 14:45:48 -0400

    privatize functions are are indirectly called

--- lib/ttt.ex
+++ lib/ttt.ex
@@ -2,25 +2,6 @@ defmodule TTT do
   alias TTT.Console.IO, as: ConsoleIO
   alias TTT.Console.Board, as: Board
 
-  def make_a_play(board, game, _status, %Player{type: :human} = turn) do
-      turn
-      |> get_move_input()
-      |> match_input()
-      |> make_a_move(board, game, turn)
-  end
-
-  def make_a_play(board, game, _status, %Player{type: :naive_computer} = turn) do
-      game
-      |> generate_naive_move(1)
-      |> make_a_move(board, game, turn)
-  end
-
-  def make_a_play(board, game, _status, %Player{type: :random_computer} = turn) do
-      game
-      |> generate_random_move()
-      |> make_a_move(board, game, turn)
-  end
-
   def play({board, game, status, turn})
     when status == :underway do
       make_a_play(board, game, status, turn)
@@ -32,19 +13,6 @@ defmodule TTT do
 
   def play({_board, _game, {_progress, outcome} = status, _turn}) when status != :underway do
     ConsoleIO.output(Messages.game_status(outcome), MessageFlags.status)
-  end
-
-  def make_a_move(move, board, game, %Player{token: token} = turn) do
-    with {:ok, game} <- Game.play_turn(game, turn, move) do
-      update_visual(board, game)
-      status = Game.status(game)
-      turn = switch_turn(game, token)
-      play({board, game, status, turn})
-    else
-      {:error, error} -> ConsoleIO.output(Messages.invalid_move(error), MessageFlags.error)
-      status = Game.status(game)
-      play({board, game, status, turn})
-    end
   end
 
   def generate_naive_move(%Game{players: %{player_one: player_one,
@@ -86,7 +54,56 @@ defmodule TTT do
     Regex.match?(~r/^[1-9]{1}$/, String.trim(input))
   end
 
-  def get_move_input(%Player{name: name, token: token} = payload) do
+  def match_input(move) do
+    move
+    |> get_row()
+    |> get_coord(move)
+  end
+
+  def update_visual(board, %Game{players: %{player_one: player_one,
+                                            player_two: player_two},
+                                            token_length: token_length} = game) do
+    player_one_moves = MapSet.to_list(game.turns[player_one.token])
+    player_two_moves = MapSet.to_list(game.turns[player_two.token])
+    moves = %{player_one: {player_one.token, player_one_moves}, player_two: {player_two.token, player_two_moves}}
+    Board.render(board, moves, token_length)
+  end
+
+  defp make_a_move(move, board, game, %Player{token: token} = turn) do
+    with {:ok, game} <- Game.play_turn(game, turn, move) do
+      update_visual(board, game)
+      status = Game.status(game)
+      turn = switch_turn(game, token)
+      play({board, game, status, turn})
+    else
+      {:error, error} -> ConsoleIO.output(Messages.invalid_move(error), MessageFlags.error)
+      status = Game.status(game)
+      play({board, game, status, turn})
+    end
+  end
+
+  #### Private function section ####
+
+  defp make_a_play(board, game, _status, %Player{type: :human} = turn) do
+      turn
+      |> get_move_input()
+      |> match_input()
+      |> make_a_move(board, game, turn)
+  end
+
+  defp make_a_play(board, game, _status, %Player{type: :naive_computer} = turn) do
+      game
+      |> generate_naive_move(1)
+      |> make_a_move(board, game, turn)
+  end
+
+  defp make_a_play(board, game, _status, %Player{type: :random_computer} = turn) do
+      game
+      |> generate_random_move()
+      |> make_a_move(board, game, turn)
+  end
+
+  defp get_move_input(%Player{name: name, token: token} = payload) do
     move = ConsoleIO.input(Messages.make_a_move(name, token), MessageFlags.request)
     case valid_input?(move) do
       true -> String.to_integer(String.trim(move))
@@ -94,12 +111,6 @@ defmodule TTT do
         ConsoleIO.output(Messages.invalid_move, MessageFlags.error)
         get_move_input(payload)
     end
-  end
-
-  def match_input(move) do
-    move
-    |> get_row()
-    |> get_coord(move)
   end
 
   defp get_row(move) do
@@ -112,14 +123,5 @@ defmodule TTT do
 
   defp get_coord(row, move) do
     {(move - 1 - (3 * row)), row}
-  end
-
-  def update_visual(board, %Game{players: %{player_one: player_one,
-                                            player_two: player_two},
-                                            token_length: token_length} = game) do
-    player_one_moves = MapSet.to_list(game.turns[player_one.token])
-    player_two_moves = MapSet.to_list(game.turns[player_two.token])
-    moves = %{player_one: {player_one.token, player_one_moves}, player_two: {player_two.token, player_two_moves}}
-    Board.render(board, moves, token_length)
   end
 end

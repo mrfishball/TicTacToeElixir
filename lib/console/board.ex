aa8b9ce1947ca3e683598693269f91f8b9eab399
Administrator <admin@Whydah.local>
2018-08-16 16:31:51 -0400

    added documentation for board module

--- lib/console/board.ex
+++ lib/console/board.ex
@@ -3,7 +3,7 @@ defmodule TTT.Console.Board do
   require Integer
 
   @moduledoc """
-    A command line representation of a game board
+    A command line representation of a game board.
   """
 
   @board_bounds 0..2
@@ -13,10 +13,10 @@ defmodule TTT.Console.Board do
     ## Parameters
 
       - left_pad, right_pad: empty space(s) around the token for scaling the board according
-      to the size of the token
+      to the size of the token.
 
     Returns a map which contains coordinates as keys and numbers (1 - 9) as values, representing
-    available spots on the board
+    available spots on the board.
   """
   def new_board(left_pad, right_pad) do
     for col <- @board_bounds, row <- @board_bounds,
@@ -24,12 +24,47 @@ defmodule TTT.Console.Board do
   end
 
   @doc """
-    @param board - the current board
-    @param list - a list of coordinates representing moves made by a player
+
+      ## Parameters
+
+      - board: Map that represents the current state of the board.
+      - list: List that contains coordinates that represent moves made by a player.
+      - token: String that represents a player's token.
+
+      Returns a map with player's token as value for each coordinate that matches those in
+      the list.
+
+      ## Examples
+
+      list = [{0, 1}, {1, 1}, {2, 1}]
+
+      %{
+        {0, 0} => "1",
+        {0, 1} => "X",
+        {0, 2} => "7",
+        {1, 0} => "2",
+        {1, 1} => "X",
+        {1, 2} => "8",
+        {2, 0} => "3",
+        {2, 1} => "X",
+        {2, 2} => "9"
+      }
+
   """
   def register_move(board, list, token) do
     Enum.reduce(list, board, fn point, acc -> Map.put(acc, point, token) end)
   end
+
+  @doc """
+
+    ## Parameters
+
+      - board: Map that represents the current state of the board.
+      - token_length: Integer that represents the lenght of the longest token.
+      It is use to make sure the row dividers are the same length as the rows.
+
+    Draws a scaled board with it's contents in command line.
+  """
 
   def show_board(board, token_length) do
     IOcontroller.output("\n")
@@ -45,13 +80,34 @@ defmodule TTT.Console.Board do
     IOcontroller.output("\n")
   end
 
+  @doc """
+
+    ## Parameters
+
+      - length: Integer that represents the lenght of the longest token.
+      It is use to make sure the row dividers are the same length as the rows.
+
+    Returns a string representing a row divider on the board.
+  """
   def draw_line(length) do
     str = String.duplicate("-", ((length + 2) * 3) + 2)
     "\n" <> str <> "\n"
   end
 
+  @doc """
+
+    ## Parameters
+
+      - board: Map that represents the current state of the board.
+      - moves: Map that contains a collection of players' moves and tokens.
+      - token_length: Integer that represents the lenght of the longest token.
+      It is use to make sure the row dividers are the same length as the rows.
+
+    Renders and draws the board in command line with tokens as values in
+    their corresponding coordinates.
+  """
   def render_board(board, %{player_one: {player_one_token, player_one_moves},
-                      player_two: {player_two_token, player_two_moves}},
+                      player_two: {player_two_token, player_two_moves}} = _moves,
                       token_length) do
     board
     |> register_move(player_one_moves, Colorizer.cyan(player_one_token))

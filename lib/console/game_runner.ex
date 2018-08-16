e5a03047926e4d09f1224d9831529dbc94cf94e5
Administrator <admin@Whydah.local>
2018-08-16 13:42:42 -0400

    removed duplicate module

--- lib/console/game_runner.ex
+++ lib/console/game_runner.ex
@@ -1,12 +1,32 @@
 defmodule TTT.Console.GameRunner do
+  alias TTT.Console.IO, as: ConsoleIO
 
   def main(_args \\ []) do
-    start()
+    new_game()
   end
 
-  def start do
-    GameMaker.show_game_menu()
+  def new_game do
+    ConsoleIO.output(Messages.title, MessageFlags.title)
+    game_menu_selection()
+    |> select_game_mode()
     |> GameMaker.assemble_game()
     |> TTT.play()
+  end
+
+  def game_menu_selection do
+    ConsoleIO.output(Messages.game_menu, MessageFlags.menu)
+    Messages.select()
+    |> ConsoleIO.input(MessageFlags.request)
+  end
+
+  def select_game_mode(choice) do
+    cond do
+      choice == "1" -> GameMaker.player_vs_player()
+      choice == "2" -> GameMaker.player_vs_computer()
+      choice == "3" -> GameMaker.computer_vs_computer()
+      true ->
+        ConsoleIO.output(Messages.invalid_entry, MessageFlags.error)
+        new_game()
+    end
   end
 end

018798c49a6cfded0110bacb2b0cb1f4dfe1dc7b
Administrator <admin@Whydah.local>
2018-08-16 14:51:21 -0400

    updated project dependencies

--- mix.exs
+++ mix.exs
@@ -49,8 +49,8 @@ defmodule TTT.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]

c4dcf41610a85d64b6b732d285679fb157f68857
Administrator <admin@Whydah.local>
2018-08-16 14:48:01 -0400

    added ex_doc dependency, logo and package detail for packaging into hex

--- mix.exs
+++ mix.exs
@@ -6,12 +6,22 @@ defmodule TTT.MixProject do
     [
       app: :ttt,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
       deps: deps(),
       escript: [main_module: GameRunner],
+
+      # Doc
+      name: "TTT",
+      source_url: "https://github.com/mrfishball/TicTacToeElixir",
+      homepage_url: "https://github.com/mrfishball/TicTacToeElixir",
+      docs: [
+        main: "TicTacToe Elixir",
+        logo: "/img/logo.jpg",
+        extras: ["README.md", "CHANGELOG.md"]
+      ]
     ]
   end
 
@@ -39,7 +49,8 @@ defmodule TTT.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]

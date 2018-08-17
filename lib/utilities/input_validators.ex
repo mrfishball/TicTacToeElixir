bbc8b7fa633ec9744c0b1e4b4b618caea6fb1264
Administrator <admin@Whydah.local>
2018-08-16 23:24:45 -0400

    added documentation to input parser and padding generator modules

--- lib/utilities/input_validators.ex
+++ lib/utilities/input_validators.ex
@@ -1,10 +1,31 @@
 defmodule TTT.Utilities.InputValidators do
 
+  @moduledoc """
+    Perform user input validation.
+  """
+
+  @doc """
+
+    ## Parameters
+
+      -input: String of user input.
+
+    Returns true if, after removing any trailing whitespaces, the input is empty.
+  """
   def empty_input?(input) do
     input = String.trim(input)
     String.length(input) < 1
   end
 
+  @doc """
+
+    ## Parameters
+
+      -input: String of user input.
+
+    Returns true if, after removing any trailing whitespaces, the input is a
+    string of digit from 1 to 9.
+  """
   def valid_move?(input) do
     Regex.match?(~r/^[1-9]{1}$/, String.trim(input))
   end
--- lib/utilities/token_padding_generator.ex
+++ lib/utilities/token_padding_generator.ex
@@ -1,19 +1,42 @@
 defmodule TTT.Utilities.TokenPaddingGenerator do
 
-  def add_paddings(longest_token, {player_one, player_two}) do
+  @moduledoc """
+    Apply paddings(whitespaces) to shorter token so that the visual board
+    scales accordingly.
+  """
+
+  @doc """
+
+    ## Parameters
+
+      -longest_token: String of the longer token.
+      -players: Tuple of players which contains player's name, token and type.
+
+    Returns a tupe of players with paddings added to the shorter token.
+  """
+  def add_paddings(longer_token, {player_one, player_two} = _players) do
     cond do
-      longest_token != player_one.token ->
-        {left_side, right_side} = generate_paddings(longest_token, player_one.token)
+      longer_token != player_one.token ->
+        {left_side, right_side} = generate_paddings(longer_token, player_one.token)
         player_one = %Player{player_one | token: "#{left_side <> player_one.token <> right_side}"}
         {player_one, player_two}
 
-      longest_token != player_two.token ->
-        {left_side, right_side} = generate_paddings(longest_token, player_two.token)
+      longer_token != player_two.token ->
+        {left_side, right_side} = generate_paddings(longer_token, player_two.token)
         player_two = %Player{player_two | token: "#{left_side <> player_two.token <> right_side}"}
         {player_one, player_two}
     end
   end
 
+  @doc """
+
+    ## Parameters
+
+      -long_token: String of the longer token.
+      -short_token: String of the shorter token.
+
+    Returns a tupe of players with paddings added to the shortest token.
+  """
   def generate_paddings(long_token, short_token) do
     difference = String.length(long_token) - String.length(short_token)
     side = div(difference, 2)

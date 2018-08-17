dda9d2268bd4bc81772535ecb08d62e9e7854b45
Administrator <admin@Whydah.local>
2018-08-17 00:05:24 -0400

    change tests to reflect changes to game module

--- test/game_test.exs
+++ test/game_test.exs
@@ -4,14 +4,14 @@ defmodule GameTest do
   test "player can make a move" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     assert {:ok, _game} = Game.play_turn(game, player_one, {0, 0})
   end
 
   test "player can't make a move if the spot is already taken" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
 
     {:ok, game} = Game.play_turn(game, player_one, {0, 0})
     assert Game.play_turn(game, player_two, {0, 0}) == {:error, :cell_taken}
@@ -23,7 +23,7 @@ defmodule GameTest do
   test "player can't make an out-of-bound move" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
 
     assert Game.play_turn(game, player_one, {4, 10}) == {:error, :out_of_bounds}
     assert Game.play_turn(game, player_two, {0, 6}) == {:error, :out_of_bounds}
@@ -33,7 +33,7 @@ defmodule GameTest do
   test "player can't play turn twice in a row" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     {:ok, game} = Game.play_turn(game, player_one, {0, 0})
 
     assert Game.play_turn(game, player_one, {2, 2}) == {:error, :not_your_turn}
@@ -42,7 +42,7 @@ defmodule GameTest do
   test "player wins horizontally (top line)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{0, 0}, {2, 1}, {1, 0}, {1, 1}, {2, 0}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -52,7 +52,7 @@ defmodule GameTest do
   test "player wins horizontally (middle line)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{0, 1}, {2, 2}, {1, 1}, {1, 0}, {2, 1}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -62,7 +62,7 @@ defmodule GameTest do
   test "player wins horizontally (bottom line)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{0, 2}, {2, 0}, {1, 2}, {1, 0}, {2, 2}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -72,7 +72,7 @@ defmodule GameTest do
   test "player wins vertically (left most)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{0, 0}, {2, 1}, {0, 1}, {1, 1}, {0, 2}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -82,7 +82,7 @@ defmodule GameTest do
   test "player wins vertically (middle)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{1, 0}, {2, 2}, {1, 1}, {2, 0}, {1, 2}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -92,7 +92,7 @@ defmodule GameTest do
   test "player wins vertically (right most)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{2, 0}, {1, 1}, {2, 1}, {1, 0}, {2, 2}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -102,7 +102,7 @@ defmodule GameTest do
   test "player wins diagonally (from top right)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{2, 0}, {2, 1}, {1, 1}, {0, 1}, {0, 2}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -112,7 +112,7 @@ defmodule GameTest do
   test "player wins diagonally (from top left)" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{0, 0}, {2, 1}, {1, 1}, {0, 1}, {2, 2}]
     game = TestHelpers.simulate_moves(game, player_one, moves, 0)
 
@@ -122,7 +122,7 @@ defmodule GameTest do
   test "a draw game" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     moves = [{0, 0}, {2, 1}, {2, 2}, {1, 1}, {0, 1}, {0, 2}, {2, 0}, {1, 0}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -136,7 +136,7 @@ defmodule GameTest do
   test "when status says 'underway', the game is still in progress" do
     player_one = Player.human({"Steven", :x})
     player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    game = Game.new_game({player_one, player_two}, 1)
     assert Game.status(game) == :underway
 
     {:ok, game} = Game.play_turn(game, player_one, {0, 1})
--- test/ttt_test.exs
+++ test/ttt_test.exs
@@ -4,9 +4,8 @@ defmodule TTTTest do
   import ExUnit.CaptureIO
 
   test "Game will automatically switch turn to 'o' after 'x' has played and vice versa" do
-    player_one = Player.human({"Steven", :x})
-    player_two = Player.human({"Connie", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    players = {Player.human({"Steven", :x}), Player.human({"Connie", :o})}
+    game = Game.new_game(players, 1)
 
     first_player = :x
     assert TTT.switch_turn(game, first_player) ==
@@ -19,9 +18,8 @@ defmodule TTTTest do
 
   test "Player tokens are updated and rendered on the board when user provides valid input" do
     board = Board.new_board("", "")
-    player_one = Player.human({"Steven", "x"})
-    player_two = Player.human({"Connie", "o"})
-    game = Game.new_game(player_one, player_two, 1)
+    players = {Player.human({"Steven", "x"}), Player.human({"Connie", "o"})}
+    game = Game.new_game({_player_one, player_two} = players, 1)
     moves = [{0, 0}, {2, 1}, {1, 0}, {1, 1}, {2, 0}]
     game = TestHelpers.simulate_moves(game, player_two, moves, 0)
 
@@ -30,9 +28,8 @@ defmodule TTTTest do
   end
 
   test "Naive computer player will always take the next open spot following opponent's move if there are no preceding spots" do
-    player_one = Player.human({"Steven", :x})
-    player_two = Player.naive_computer({"Comp", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    players = {Player.human({"Steven", :x}), Player.naive_computer({"Comp", :o})}
+    game = Game.new_game({player_one, _player_two} = players, 1)
 
     {:ok, game} = Game.play_turn(game, player_one, {0, 0})
 
@@ -40,9 +37,8 @@ defmodule TTTTest do
   end
 
   test "Naive computer player will always take the first available spot preceding the opponent's move" do
-    player_one = Player.human({"Steven", :x})
-    player_two = Player.naive_computer({"Comp", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    players = {Player.human({"Steven", :x}), Player.naive_computer({"Comp", :o})}
+    game = Game.new_game({player_one, _player_two} = players, 1)
 
     {:ok, game} = Game.play_turn(game, player_one, {2, 0})
 
@@ -51,9 +47,8 @@ defmodule TTTTest do
 
   test "Random computer will generate random moves" do
     :rand.seed(:exs1024, {123, 123_534, 345_345})
-    player_one = Player.human({"Steven", :x})
-    player_two = Player.random_computer({"Comp", :o})
-    game = Game.new_game(player_one, player_two, 1)
+    players = {Player.human({"Steven", :x}), Player.random_computer({"Comp", :o})}
+    game = Game.new_game({player_one, _player_two} = players, 1)
 
     {:ok, game} = Game.play_turn(game, player_one, {2, 0})
 

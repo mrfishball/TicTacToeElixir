a70d4895de4a6df3d6edc34ce47f3e9a080e12ba
Administrator <admin@Whydah.local>
2018-08-17 00:37:36 -0400

    added documentation

--- lib/IOcontroller.ex
+++ lib/IOcontroller.ex
@@ -1,6 +1,20 @@
 defmodule IOcontroller do
   alias TTT.Console.Cli, as: Cli
 
+  @moduledoc """
+    This is a controller module that relays input and output to corresponding handlers.
+    The handlers can be changed without having to change any code in the game logic.
+  """
+
+  @doc """
+
+    ## Parametrs
+      - payload: String of user input.
+      - type (Optional): String of categories of input. For example, "error", "request", etc.
+      - source: Handler function injected.
+
+    The return data and type are determined by the handler function.
+  """
   def input(payload, type \\ None, source \\ &Cli.input/2) do
     source.(payload, type)
   end

b280515aff2b823180cbc065b72db0c316d32504
Administrator <admin@Whydah.local>
2018-08-20 15:12:41 -0500

    most command line io migrated to game runner

--- lib/console/game_runner.ex
+++ lib/console/game_runner.ex
@@ -1,4 +1,5 @@
 defmodule TTT.Console.GameRunner do
+  alias TTT.Console.Cli, as: Cli
 
   @moduledoc """
     A command line game runner.
@@ -13,7 +14,7 @@ defmodule TTT.Console.GameRunner do
     Display a game title, menu and asking for game mode input to properly setup the game.
   """
   def new_game do
-    IOcontroller.output(Messages.title, MessageTags.title)
+    Cli.output(Messages.title, MessageTags.title)
     game_menu_selection()
     |> select_game_mode()
     |> GameMaker.polish_tokens_and_paddings()
@@ -22,19 +23,78 @@ defmodule TTT.Console.GameRunner do
   end
 
   defp game_menu_selection do
-    IOcontroller.output(Messages.game_menu, MessageTags.menu)
+    Cli.output(Messages.game_menu, MessageTags.menu)
     Messages.select()
-    |> IOcontroller.input(MessageTags.request)
+    |> Cli.input(MessageTags.request)
+  end
+
+  defp computer_type_selection(computer_name) do
+    Cli.output(Messages.computer_choice_menu(computer_name), MessageTags.menu)
+    Messages.select()
+    |> Cli.input(MessageTags.request)
+    |> get_computer_type(computer_name)
+  end
+
+  defp get_computer_type(choice, computer_name) do
+    cond do
+      choice == "1" -> &Player.naive_computer/1
+      choice == "2" -> &Player.random_computer/1
+      true ->
+        Cli.output(Messages.invalid_entry, MessageTags.error)
+        computer_type_selection(computer_name)
+    end
   end
 
   defp select_game_mode(choice) do
     cond do
-      choice == "1" -> GameMaker.player_vs_player()
-      choice == "2" -> GameMaker.player_vs_computer()
-      choice == "3" -> GameMaker.computer_vs_computer()
+      choice == "1" -> player_vs_player()
+      choice == "2" -> player_vs_computer()
+      choice == "3" -> computer_vs_computer()
       true ->
-        IOcontroller.output(Messages.invalid_entry, MessageTags.error)
+        Cli.output(Messages.invalid_entry, MessageTags.error)
         new_game()
+    end
+  end
+
+  defp player_vs_player do
+    player_one = GameMaker.setup_player(get_player_name(1), get_player_symbol(1), &Player.human/1)
+    player_two = GameMaker.setup_player(get_player_name(2), get_player_symbol(2), &Player.human/1)
+    {player_one, player_two}
+  end
+
+  defp player_vs_computer do
+    player_one = GameMaker.setup_player(get_player_name(1), get_player_symbol(1), &Player.human/1)
+    player_two = make_computer_player(get_player_name(2), get_player_symbol(2))
+    {player_one, player_two}
+  end
+
+  defp computer_vs_computer do
+    player_one = make_computer_player(get_player_name(1), get_player_symbol(1))
+    player_two = make_computer_player(get_player_name(2), get_player_symbol(2))
+    {player_one, player_two}
+  end
+
+  defp make_computer_player(player_name, player_token) do
+    GameMaker.setup_player(player_name, player_token, computer_type_selection(player_name))
+  end
+
+  defp get_player_name(player_number) do
+    name = Cli.input(Messages.player_name(player_number), MessageTags.request)
+    with {:ok, name} <- GameMaker.set_player_name(name) do
+      name
+    else
+      {:error, name} -> Cli.output(Messages.invalid_name(name), MessageTags.error)
+      get_player_name(player_number)
+    end
+  end
+
+  defp get_player_symbol(player_number) do
+    symbol = Cli.input(Messages.player_symbol(player_number), MessageTags.request)
+    with {:ok, token} <- GameMaker.set_player_symbol(symbol) do
+      token
+    else
+      {:error, token} -> Cli.output(Messages.invalid_token(token), MessageTags.error)
+      get_player_symbol(player_number)
     end
   end
 end

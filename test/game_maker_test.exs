ae9b1174548b651c32526082d81c561ff1496a85
Administrator <admin@Whydah.local>
2018-08-20 15:29:11 -0500

    new tests for improved functions

--- test/game_maker_test.exs
+++ test/game_maker_test.exs
@@ -1,5 +1,23 @@
 defmodule GameMakerTest do
   use ExUnit.Case
-  import ExUnit.CaptureIO
+
+  test "player is setup with the correct type" do
+    type = &Player.human/1
+    player = GameMaker.setup_player("Steven", "X", type)
+    assert player.type == :human
+  end
+
+  test "player is setup with the correct name" do
+    name = "Steven"
+    player = GameMaker.setup_player(name, "O", &Player.human/1)
+    assert player.name == "Steven"
+  end
+
+  test "player is setup with the correct token" do
+    token = "D"
+    player = GameMaker.setup_player("Steven", token, &Player.human/1)
+    assert player.token == "D"
+  end
+
 
 end

27baa3b24a32cecc5555fdebbeddbc700599f888
mrfishball <mrfishball@gmail.com>
2018-08-31 15:51:10 -0400

    updated project dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TTT.MixProject do
   def project do
     [
       app: :ttt,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
